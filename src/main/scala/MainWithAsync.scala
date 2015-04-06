6849a9b4fa21f331e74ac0be702205b47c7dc36c
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-04-06 17:52:36 +0900

    がくぞさん指摘取り込み

--- src/main/scala/MainWithAsync.scala
+++ src/main/scala/MainWithAsync.scala
@@ -13,7 +13,8 @@ object MainWithAsync extends App {
   AsyncConnectionPool.singleton("jdbc:mysql://localhost:3306/foo", "root", "")
 
   // normalCreate 公式サンプルの例。正しくsession維持される。Actorではない。
-  akkaCreate // Actor版　吉田さん指摘により正しく動くようになった。
+  // akkaCreateXuweikVer // Actor版　吉田さん指摘により正しく動くようになった。
+  akkaCreateGakuzzzzVer // Actor版　がくぞさん指摘取り込み txを使うのが良さそう？
 
   private def normalCreate = {
     // create a new record within a transaction
@@ -29,7 +30,7 @@ object MainWithAsync extends App {
     Await.result(created, 5.seconds)
   }
 
-  private def akkaCreate = {
+  private def akkaCreateXuweikVer = {
     implicit val timeout = Timeout(60, TimeUnit.SECONDS)
     val system = ActorSystem("mySystem")
     val cActor = system.actorOf(Props[CompanyActor], "cActor")
@@ -47,13 +48,36 @@ object MainWithAsync extends App {
     catch { case e:Exception => println(e.getMessage)}
     system.shutdown()
   }
+
+  private def akkaCreateGakuzzzzVer = {
+    implicit val timeout = Timeout(60, TimeUnit.SECONDS)
+    val system = ActorSystem("mySystem")
+    val cActor = system.actorOf(Props[CompanyActor], "cActor")
+    val tx = AsyncDB.localTx { implicit tx =>
+
+      for {
+        c <- cActor.ask(MessageWithGakuzzzz("ScalikeJDBC, Inc." + DateTime.now.getMillis, tx))
+        c <- cActor.ask(MessageWithGakuzzzz("ScalikeJDBC, Inc." + DateTime.now.getMillis, tx))
+        c <- cActor.ask(MessageWithGakuzzzz("ScalikeJDBC, Inc." + DateTime.now.getMillis, tx))
+        c <- cActor.ask(MessageWithGakuzzzz("ScalikeJDBC, Inc." + DateTime.now.getMillis, tx))
+        c <- cActor.ask(MessageWithGakuzzzz("ScalikeJDBC, Inc." + DateTime.now.getMillis, tx))
+      } yield()
+    }
+
+    try Await.result(tx, 5.seconds)
+
+    catch { case e:Exception => println(e.getMessage)}
+    system.shutdown()
+  }
 }
 final case class Message(name: String, tx: TxAsyncDBSession)
+final case class MessageWithGakuzzzz(name: String, tx: TxAsyncDBSession)
+
 class CompanyActor extends Actor {
-  implicit val session = AsyncDB.sharedSession
   def receive = {
     case Message(name, tx) =>
-      println(session.toString)
+      Company.create(name, Some("http://scalikejdbc.org/"))(tx, implicitly).pipeTo(sender())
+    case MessageWithGakuzzzz(name, tx) =>
       Company.create(name, Some("http://scalikejdbc.org/"))(tx, implicitly).pipeTo(sender())
   }
 }
